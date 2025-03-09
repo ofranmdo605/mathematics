a26675364120963fa1f5eee86eb57f8b3b0f7f13
ofranmdo605 <ofranmdo605@gmail.com>
2025-03-09 19:18:13 +0000

    Add 4.ts

---
+++ src/4.ts
@@ -0,0 +1,8 @@
+// Define a function to calculate the area of a rectangle
+function calculateArea(width, height) {
+  return width * height;
+}
+
+// Use the function with different values for width and height
+console.log("The area of a rect with w:10 h:5 is:", calculateArea(10, 5));
+console.log("The area of a rect with w:4 h:2 is:", calculateArea(4, 2));
