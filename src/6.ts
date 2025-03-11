116ad937a0d13fcca3a9a2acd2b36cdb0053a78a
ofranmdo605 <ofranmdo605@gmail.com>
2025-03-11 16:57:45 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Mathematics</title>
+</head>
+<body>
+  <h1>Exploring mathematical concepts and formulas.</h1>
+  <p>The square of the hypotenuse of a right triangle is equal to the sum of the squares of the other two sides.</p>
+  <form>
+    <label for="side-a">Side A:</label>
+    <input type="number" id="side-a" name="side-a"><br><br>
+    <label for="side-b">Side B:</label>
+    <input type="number" id="side-b" name="side-b"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
