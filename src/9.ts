bac4249f866a285edb3de7aaa67457004e8fcd47
ofranmdo605 <ofranmdo605@gmail.com>
2025-03-14 01:02:58 +0000

    Add 9.ts

---
+++ src/9.ts
@@ -0,0 +1,16 @@
+
+// Example 1: Calculating the area of a circle using pi
+const pi = 3.14;
+function calculateArea(radius) {
+return pi * radius ** 2;
+}
+
+// Example 2: Checking if a number is even or odd
+function isEven(number) {
+return (number % 2 === 0);
+}
+
+// Example 3: Converting Celsius to Fahrenheit
+function convertToFahrenheit(celsius) {
+return celsius * 1.8 + 32;
+}
