71e62680374c3f7eca18a65183b1cb233c3f989f
ofranmdo605 <ofranmdo605@gmail.com>
2025-03-31 00:07:07 +0000

    Add 22.ts

---
+++ src/22.ts
@@ -0,0 +1,8 @@
+def calculate_area(radius, height):
+    area = 3.14 * radius ** 2 * height
+    return area
+
+radius = 5
+height = 10
+area = calculate_area(radius, height)
+print(area)  # Output: 785.3975018104516
