1a54866a3ee343127dcc05c768e79ffe4292cdc5
ofranmdo605 <ofranmdo605@gmail.com>
2025-05-26 05:10:15 +0000

    Add 45.ts

---
+++ src/45.ts
@@ -0,0 +1,9 @@
+def simple_math():
+    # Example of basic arithmetic operations
+    print(5 + 3)  # Addition
+    print(2 - 4)  # Subtraction
+    print(7 * 8)  # Multiplication
+    print(10 / 3)  # Division
+    print(9 ** 2)  # Exponentiation
+
+simple_math()
